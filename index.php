aa41972577857907a8c64c73e6c6ab9e5cd46165
unknown <taffarel-182@hotmail.com>
2020-01-12 15:38:10 -0400

    inserir e alterar

--- index.php
+++ index.php
@@ -5,24 +5,31 @@ require_once("config.php");
 //carrega um usuario
 //$root = new Usuario();
 //$root->loadbyId(9);
-
 //echo $root;
 
 //carrega uma lista de usuarios
-
 //$lista = Usuario::getList();
-
 //echo json_encode($lista);
 
 //carrega uma lista de usuarios buscando pelo login
 //$search = Usuario::search("ad");
-
 //echo json_encode($search);
 
 //carrega um usuario usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("jose", "123456");
+//echo $usuario;
+
+//criando um novo usuario
+//$aluno = new Usuario("aluno", "@lun0");
+//$aluno->insert();
+//echo $aluno;
+
 $usuario = new Usuario();
-$usuario->login("jose", "123456");
+
+$usuario->loadById(11);
+
+$usuario->update("professor", "!@#$%");
 
 echo $usuario;
-
 ?>
